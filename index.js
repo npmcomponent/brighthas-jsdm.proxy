f061b1513102338a9cfe62ffbe56ec39d4025390
brighthas <brighthas@gmail.com>
2013-04-02 10:28:41 +0800

    publish version 0.0.1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var jq = require("jquery");
 var Emit  = require("emitter");
-    
-    function Proxy(url){
+function Proxy(url){
         
         this._emitter = new Emit;
         this._url = url;
@@ -11,16 +10,18 @@ var Emit  = require("emitter");
         function loop(){
             setTimeout(function(){
                 var esk = Object.keys(self._events);
+                
                 if(esk.length === 0);
-                else{
+                else{ 
+
                     jq.post(self._url,{method:"on",events:JSON.stringify(self._events)},function(rs){
-               
                         for(var en in rs){
                             if(self._events[en]){
                                 var t = Date.now();
                                 self._events[en] = t;
                             }
-                            self._emitter.emit(en,rs[en]);
+                            self._emitter.emit.apply(self._emitter,rs[en]);
+                            
                         }
                         loop();
                         
@@ -46,6 +47,11 @@ var Emit  = require("emitter");
           jq.post(this._url,{method:"exec",commandName:commandName,args:JSON.stringify(args)},callback)
         },
         
+        call:function(methodName,id,args,callback){
+          args = args?args:[];
+          jq.post(this._url,{method:"call",methodName:methodName,id:id,args:JSON.stringify(args)},callback) 
+        },
+        
         query:function(name,args,callback){
           jq.post(this._url,{method:"query",queryName:name,args:JSON.stringify(args)},callback);
         },
@@ -63,6 +69,7 @@ var Emit  = require("emitter");
             this._emitter.off.apply(this._emitter,arguments);
         }
         
-    }
+}
     
 module.exports  =  Proxy;
+
