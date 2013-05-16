30bbfc6b57a6430ecfb6181c9d27d22f0031e020
brighthas <brighthas@gmail.com>
2013-05-16 14:59:35 +0800

    use socket.io

--- index.js
+++ index.js
@@ -10,12 +10,28 @@ function Proxy(socket,domainName){
     // for query
     var emitter2 = this._emitter2 = new Emit;
 
+    // for exec
+    var emitter3 = this._emitter3 = new Emit;
+
+    // for call
+    var emitter4 =  this._emitter4 = new Emit;
+
     this._socket.on(this._domainName+"-query-result",function(rs){
-        emitter2.emit(rs.qid,rs.data);
+        emitter2.emit.apply(emitter2,rs);
+    })
+
+
+    this._socket.on(this._domainName+"-exec-result",function(rs){
+        emitter3.emit.apply(emitter3,rs);
+    })
+
+
+    this._socket.on(this._domainName+"-call-result",function(rs){
+        emitter4.emit.apply(emitter4,rs);
     })
 
     this._socket.on(this._domainName+"-emit",function(event){
-         emitter.emit.apply(emitter,event);
+        emitter.emit.apply(emitter,event);
     })
 
 }
@@ -40,12 +56,16 @@ Proxy.prototype = {
             this.socket.emit(this._domainName+"-once",eventname);
         },
         
-        exec:function(commandName,args){
-            this.socket.emit(this._domainName+"-exec",{commandName:commandName,args:args});
+        exec:function(commandName,args,callback){
+            var commandId = uuid();
+            this._emitter3.once(commandId,callback);
+            this.socket.emit(this._domainName+"-exec",{commandId:commandId,commandName:commandName,args:args});
         },
         
-        call:function(methodName,id,args){
-            this.socket.emit(this._domainName+"-call",{methodName:methodName,id:id,args:args});
+        call:function(methodName,id,args,callback){
+            var callId = uuid();
+            this._emitter4.once(callId,callback);
+            this.socket.emit(this._domainName+"-call",{callId:callId,methodName:methodName,id:id,args:args});
         }
         
 }
